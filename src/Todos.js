5dc8289c1eff23befc86a0744fa5bdeb01cbd340
Jena Boehm <boehmjena@gmail.com>
2020-11-12 10:56:49 -0800

    Add task

--- src/Todos.js
+++ src/Todos.js
@@ -3,10 +3,16 @@ import request from 'superagent';
 
 export default class Todos extends Component {
     state = {
-        todos: []
+        todos: [],
+        todo: ''
     }
 
     componentDidMount = async () => {
+        await this.fetchTodos()
+    }
+
+
+    fetchTodos = async() => {
         const response = await request
             .get('https://pacific-mesa-57017.herokuapp.com/api/todos')
             .set('Authorization', this.props.token)
@@ -14,16 +20,40 @@ export default class Todos extends Component {
             this.setState({ todos: response.body })
     }
 
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await request
+            .post('https://pacific-mesa-57017.herokuapp.com/api/todos')
+            .send({
+                todo: this.state.todo
+                })
+            .set('Authorization', this.props.token)
+
+            await this.fetchTodos();
+
+    }
+
     render() {
         return (
             <div>
                 <div className='title'>ToDo List</div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <span>Add a task:</span>
+                        <input
+                            value={this.state.todo}
+                            onChange={(e) => 
+                                this.setState({ todo: e.target.value})}
+                        />
+                    </label>
+                    <button>Add Task</button>
+                </form>
                 {
                     Boolean(this.state.todos.length) && 
                     this.state.todos.map(todo => 
                     <div className="single-task">
                         <div className="task">Task: {todo.todo}</div>
-                        <div className="completed">Completed: {todo.completed}</div>
                     </div>)
                 }
             </div>
