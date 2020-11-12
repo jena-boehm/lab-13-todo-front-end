d3cde59d26346442c137306ed308d4e97a2531fc
Jena Boehm <boehmjena@gmail.com>
2020-11-11 16:09:56 -0800

    form set state

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 
 export default class SignUp extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div>
-                Sign Up
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <span>Email:</span>
+                        <input 
+                            onChange={(e) => 
+                                this.setState({ email: e.target.value })} 
+                            value={this.state.email} 
+                        />
+                    </label>
+                    <label>
+                        <span>Password:</span>
+                        <input 
+                            onChange={(e) =>
+                                this.setState({ password: e.target.value })}
+                            value={this.state.password}
+                            type="password"
+                        />
+                    </label>
+                    <button>Sign Up</button>
+                </form>
             </div>
         )
     }
