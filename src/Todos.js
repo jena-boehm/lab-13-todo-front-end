1ebce7b606902f6119a6087216d6f080afb0ed0f
Jena Boehm <boehmjena@gmail.com>
2020-11-12 11:23:22 -0800

    Add working complete button

--- src/Todos.js
+++ src/Todos.js
@@ -4,7 +4,8 @@ import request from 'superagent';
 export default class Todos extends Component {
     state = {
         todos: [],
-        todo: ''
+        todo: '',
+        loading: false
     }
 
     componentDidMount = async () => {
@@ -13,11 +14,12 @@ export default class Todos extends Component {
 
 
     fetchTodos = async() => {
+        await this.setState({loading: true });
         const response = await request
             .get('https://pacific-mesa-57017.herokuapp.com/api/todos')
             .set('Authorization', this.props.token)
 
-            this.setState({ todos: response.body })
+        await this.setState({ todos: response.body, loading: false })
     }
 
     handleSubmit = async (e) => {
@@ -31,7 +33,16 @@ export default class Todos extends Component {
             .set('Authorization', this.props.token)
 
             await this.fetchTodos();
+            await this.setState({ loading: true })
+            // STRETCH: clear form on submit
+    }
 
+    handleCompletedClick = async (id) => {
+        await request
+            .put(`https://pacific-mesa-57017.herokuapp.com/api/todos/${id}`)
+            .set('Authorization', this.props.token);
+            
+        await this.fetchTodos();
     }
 
     render() {
@@ -49,12 +60,30 @@ export default class Todos extends Component {
                     </label>
                     <button>Add Task</button>
                 </form>
+
+                <div className='title'>Tasks</div>
                 {
-                    Boolean(this.state.todos.length) && 
-                    this.state.todos.map(todo => 
-                    <div className="single-task">
-                        <div className="task">Task: {todo.todo}</div>
-                    </div>)
+                    this.state.loading
+                    ? 'Loading...'
+                    : this.state.todos.map(todo => 
+                        <div 
+                            className="single-task"
+                            key={`${todo.todo}${todo.id}${Math.random()}`}
+                            style={{textDecoration: todo.completed
+                                ? 'line-through'
+                                : 'none'}
+                                }>
+                            <ul>
+                                <li>
+                                    <span className="task">{todo.todo} </span>
+                                    {
+                                        todo.completed
+                                        ? ''
+                                        : <button onClick={() => this.handleCompletedClick(todo.id)}>Complete</button>
+                                    }
+                                </li>
+                            </ul>
+                        </div>)
                 }
             </div>
         )
